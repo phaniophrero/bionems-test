5334aab16abe64e6482171b84a072071bf1ec8a1
phaniophrero <phaniophrero@yahoo.com>
2021-09-12 00:11:36 +0200

    added env variables and gitingnore file

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(%yp8a*0&8ya7h_u1=z_494ua6bltzuzg(#_bafg_7&7iif3ek'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
