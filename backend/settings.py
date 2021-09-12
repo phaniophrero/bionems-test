cf05f06b52c4e12386c5cfe573da5b8dd36bf9b7
phaniophrero <phaniophrero@yahoo.com>
2021-09-13 01:26:39 +0200

    update settings and App files

--- backend/settings.py
+++ backend/settings.py
@@ -150,21 +150,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'frontend/build/static',
-]
-
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'frontend/build/static'),
+#     BASE_DIR / 'static',
+#     BASE_DIR / 'frontend/build/static',
 # ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import ReactGa from "react-ga";
 
@@ -14,6 +14,7 @@ import Desserts from "./components/Products/Desserts";
 import ProductPage from "./pages/ProductPage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+// import Slider from "./components/Slider/Slider";
 // import NavbarCategories from "./components/Navbar/NavbarCategories";
 
 function App() {
@@ -29,16 +30,13 @@ function App() {
     <>
       <Navbar />
       <Layout>
+        {/* <Slider /> */}
+        {/* <NavbarCategories/> */}
         <Route exact path="/" component={Home} />
-
         <Route exact path="/" component={Nems} />
-
         <Route path="/bobuns" component={Bobuns} />
-
         <Route path="/salades" component={Salades} />
-
         <Route path="/desserts" component={Desserts} />
-
         <Route path="/boissons" component={Boissons} />
       </Layout>
       <Route path="/product/:id" component={ProductPage} />
