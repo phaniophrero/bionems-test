1b18508bbf185e6ebf0916953e9dc565df8f0676
Niculae Alexandru Stefanel <phaniophrero@yahoo.com>
2021-10-26 02:18:20 +0200

    settings file updated

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ import os
 from pathlib import Path
 # import django_heroku
 # import dj_database_url
-import environ
+# import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,18 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-(%yp8a*0&8ya7h_u1=z_494ua6bltzuzg(#_bafg_7&7iif3ek'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PRODUCTION = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1',
-                 'localhost', '217.69.3.26', 'vercel.app', 'bionems-dj-react.herokuapp.com']
+                 'localhost', '217.69.3.26', 'bionems-dj-react.herokuapp.com']
 
 
 # Application definition
@@ -137,10 +137,10 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME':  'db_bionems',
-        'USER': env('DB_USER'),
-        'PASSWORD':  env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432'
+        'USER': '',
+        'PASSWORD':  '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
@@ -212,32 +212,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if PRODUCTION:
-    DEBUG = False
-    ALLOWED_HOSTS = ['$HOST']
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'db_bionems',
-            'USER': 'dbuser',
-            'PASSWORD': env('DB_PASSWORD'),
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = '/var/www/mysite/myenv/mysite/media/'
-else:
-    DEBUG = True
-    ALLOWED_HOSTS = []
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-        os.path.join(BASE_DIR, 'frontend/build/static'),
-    ]
+try:
+    from .local_settings import *
+except ImportError:
+    pass
